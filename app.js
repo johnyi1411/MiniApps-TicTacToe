0d121f93a67759ad8b68cb74ae6082298fee991d
John Yi <john.yi1411@gmail.com>
2019-08-12 14:17:54 -0700

    Complete initial implementation of tictactoe

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var gameOn = true;
 var currentPlayer = 'X';
 
 var takeSquare = (id, player) => {
-  if (id) {
+  if (id && id.length < 2) {
     document.getElementById(id).innerHTML = player;
   }
 };
@@ -22,26 +22,65 @@ document.addEventListener('click', (event) => {
       console.log(board);
       checkWinner();
     }
+  } else if (event.path[0].id === 'clear') {
+    gameOn = true;
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    currentPlayer = 'X';
+    for (let i = 0; i < 9; i++) {
+      document.getElementById(i).innerHTML = '';
+    }
+    document.getElementById('winner').innerHTML = '';
   }
 });
 
 var checkWinner = () => {
+  //check rows for winner
   for (let row = 0; row < 3; row++) {
     if (board[0 + (row * 3)] === board[1 + (row * 3)] && board[1 + (row * 3)] === board[2 + (row * 3)] && board[0 + (row * 3)] !== 0) {
       gameOn = false;
-      document.getElementsByClassName('winner').innerHTML = `Winner! Player ${board[0 + (row * 3)]}`;
-      console.log(`Winner! Player ${board[0 + row]}`);
+      document.getElementById('winner').innerHTML = `Winner! Player ${board[0 + (row * 3)]}`;
+      return;
     }
   }
+  //check columns for winner
   for (let column = 0; column < 3; column++) {
     if (board[0 + column] === board[3 + column] && board[0 + column] === board[6 + column] && board[0 + column] !== 0) {
       gameOn = false;
-      document.getElementsByClassName('winner').innerHTML = `Winner! Player ${board[0 + column]}`;
-      console.log(`Winner! Player ${board[0 + column]}`);
+      document.getElementById('winner').innerHTML = `Winner! Player ${board[0 + column]}`;
+      return;
     }
   }
+  //check diagonal for winner
+  if (board[0] === board[4] && board[4]) {
+    if (board[4] === board[8]) {
+      gameOn = false;
+      document.getElementById('winner').innerHTML = `Winner! Player ${board[4]}`;
+      return;
+    }
+  } else if (board[2] === board[4] && board[4]) {
+    if (board[4] === board[6]) {
+      gameOn = false;
+      document.getElementById('winner').innerHTML = `Winner! Player ${board[4]}`;
+      return;
+    }
+  }
+  //check for tie
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === 0) {
+      checkBoard();
+      return;
+    }
+  }
+  gameOn = false;
+  document.getElementById('winner').innerHTML = `Tie!`;
+  checkBoard();
+};
+
+var checkBoard = () => {
   console.log(`\n${board[0]}, ${board[1]}, ${board[2]}\n
   ${board[3]}, ${board[4]}, ${board[5]}\n
   ${board[6]}, ${board[7]}, ${board[8]}\n`);
 };
+
+
 
